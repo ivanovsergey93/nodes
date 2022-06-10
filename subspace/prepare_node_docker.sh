6019ec71ac35a7ab7e8311164cd0f61054a99943
Sergey Ivanov <ivanovsergey93@gmail.com>
2022-06-11 01:02:24 +0300

    Updated preparation script

--- subspace/prepare_node_docker.sh
+++ subspace/prepare_node_docker.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 cd $HOME
 
-sed -i 's/35G/100G/g' .bash_profile
 source ~/.bash_profile
-
-wget https://raw.githubusercontent.com/ivanovsergey93/nodes/main/subspace/remove_old_node_nodes_guru.sh
-chmod +x remove_old_node_nodes_guru.sh
-./remove_old_node_nodes_guru.sh
 
 mkdir subspace
 cd subspace
@@ -16,10 +11,6 @@ chmod a+rw -R farmer-data
 chmod a+rw -R node-data
 wget https://raw.githubusercontent.com/ivanovsergey93/nodes/main/subspace/docker-compose.yml
 
-touch .env
-printf 'SUBSPACE_NODENAME="%s"\n' "$SUBSPACE_NODENAME" > .env
-printf 'SUBSPACE_WALLET="%s"\n' "$SUBSPACE_WALLET" >> .env
-printf 'SUBSPACE_PLOT="%s"\n' "$SUBSPACE_PLOT" >> .env
 
 cd $HOME
 wget https://raw.githubusercontent.com/ivanovsergey93/nodes/main/install_docker.sh
