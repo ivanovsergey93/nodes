25951ef9906582a3505b860c571522ef076e0981
Sergey <sergey@windows11.com>
2022-06-13 20:40:46 +0300

    update_starknet

---
+++ starknet/update_starknet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/pathfinder
+git fetch
+git checkout v0.2.2-alpha
+cargo build --release --bin pathfinder
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+cd py
+source .venv/bin/activate
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+systemctl restart starknetd
