1fb199a7c53ecc325cebcfea84f844b09d00819b
Sergey <sergey@windows11.com>
2022-06-24 10:14:00 +0300

    update starknet

--- starknet/update_starknet.sh
+++ starknet/update_starknet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/pathfinder
 git fetch
-git checkout v0.2.2-alpha
+git checkout v0.2.3-alpha
 cargo build --release --bin pathfinder
 mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 cd py
