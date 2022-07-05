dfa56d6acc38302149c4e8fef297d5c73c8ee080
Sergey <sergey@windows11.com>
2022-07-05 19:43:07 +0300

    update starknet

--- starknet/update_starknet.sh
+++ starknet/update_starknet.sh
@@ -6,6 +6,7 @@ cargo build --release --bin pathfinder
 mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 cd py
 source .venv/bin/activate
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
 PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
 systemctl restart starknetd
 pathfinder -V
