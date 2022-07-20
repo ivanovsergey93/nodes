b449e1ea809751bdd4526cb73ea38e9607594524
Sergey <sergey@windows11.com>
2022-07-20 20:29:47 +0300

    update starknet

--- starknet/update_starknet.sh
+++ starknet/update_starknet.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd ~/pathfinder
 git fetch
-git checkout v0.2.5-alpha
-cargo build --release --bin pathfinder
-mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+git checkout v0.2.6-alpha
 cd py
 source .venv/bin/activate
 PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
 PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+cd ~/pathfinder
+
+cargo build --release --bin pathfinder
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+
 systemctl restart starknetd
 pathfinder -V
