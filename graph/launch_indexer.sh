d0635d8fbf8acc180dcdc761e53432c73d23fd4a
Sergey <ivanovsergey93@gmail.com>
2022-10-03 23:06:16 +0300

    launch indexer update

--- graph/launch_indexer.sh
+++ graph/launch_indexer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-bash <(curl -s https://raw.githubusercontent.com/ivanovsergey93/nodes/main/install_docker.sh)
+#nano /etc/needrestart/needrestart.conf $nrconf{restart} = 'l'; 
+sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
+
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
 
 mkdir -p $HOME/graph-indexer
 wget -O $HOME/graph-indexer/docker-compose.yml https://raw.githubusercontent.com/ivanovsergey93/nodes/main/graph/docker-compose.yml
@@ -8,3 +12,7 @@ wget -O $HOME/graph-indexer/prometheus.yml https://raw.githubusercontent.com/iva
 
 cd $HOME/graph-indexer/
 docker-compose up -d
+
+#wget -O caddy.sh https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/caddy.sh && chmod +x caddy.sh && ./caddy.sh
+
+#curl ifconfig.me
